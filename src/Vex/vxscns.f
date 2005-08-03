57d3a4c8e8465daec62a2e7becd6fc0e925f21b3
reynolds <reynolds@d0039845-cc32-0410-9ae4-e027caa9de09>
2005-08-03 14:03:49 +0000

    Allow use of modes with FORMAT=NONE (see eg3mmb.key). Add checks in various places to make sure that these modes are skipped and in the SCHED section replace the mode of the FORMAT=NONE scans with the format of a recording scan. This was a fairly big overhaul and I hope I haven't broken anything.
    git-svn-id: https://svn.aoc.nrao.edu/repos/sched/trunk/sched@34 d0039845-cc32-0410-9ae4-e027caa9de09

--- src/Vex/vxscns.f
+++ src/Vex/vxscns.f
@@ -20,8 +20,8 @@ C
 C
       INTEGER OLDSCN, PSETI, OLDIPS, FRSTSCN
       INTEGER VXMDIFP(MFSET,MPSET)
-      INTEGER ISCN, ISETFL, IMODE, I, IFS, IPS, J
-      INTEGER ISTA, NMDORI
+      INTEGER ISCN, ISETFL, IMODE, I, IFS, IPS, J, JSCN, RECSCN
+      INTEGER ISTA, NMDORI, VXGTST, ISET
       CHARACTER CALSET*4, CALSCN*4
 C ----------------------------------------------------------------------
       IF( DEBUG ) CALL WLOG( 1, 'VXSCNS: Starting.' )
@@ -52,137 +52,171 @@ C
 C
          MODSCN(ISCN) = IMODE
 C
-C        The VEX modes are the same across antennas, but some
-C        antennas may join later, so find the "oldest" freq mode
-C        or an IPS change, set IFS and IPS to A valid number
+C        If this Mode uses FORMAT=NONE, then replace the mode with
+C        the mode of the previous recording scan and do nothing more
+         ISET = VXGTST( IMODE )
+         IF( FORMAT(ISET)(1:4) .NE. 'NONE' ) THEN
 C
-         FRSTSCN = MAXSCN + 1
-         IPS = 0
-         DO ISTA = 1, NSTA
-            IF( STASCN(ISCN,ISTA) ) THEN  
-               IF (FSETSCN(FSETI(ISCN,ISTA)).LT.FRSTSCN) THEN
-                  IFS = FSETI(ISCN,ISTA)
-                  FRSTSCN = FSETSCN(IFS)
-               END IF
-               IF (IPS .EQ. 0) IPS = PSETI(ISCN,ISTA)
-               IF (IPS.GT.MPSET.OR.IFS.GT.MFSET) THEN
-                  CALL ERRLOG('VXSCNS: Too many frequency'//
-     .                ' mode or Pcal changes ')
-                  STOP
-               END IF
+C          The VEX modes are the same across antennas, but some
+C          antennas may join later, so find the "oldest" freq mode
+C          or an IPS change, set IFS and IPS to A valid number
 C
-C              I can only assume this was a leftover Huib v75
+           FRSTSCN = MAXSCN + 1
+           IPS = 0
+           DO ISTA = 1, NSTA
+              IF( STASCN(ISCN,ISTA) ) THEN  
+                 IF (FSETSCN(FSETI(ISCN,ISTA)).LT.FRSTSCN) THEN
+                    IFS = FSETI(ISCN,ISTA)
+                    FRSTSCN = FSETSCN(IFS)
+                 END IF
+                 IF (IPS .EQ. 0) IPS = PSETI(ISCN,ISTA)
+                 IF (IPS.GT.MPSET.OR.IFS.GT.MFSET) THEN
+                    CALL ERRLOG('VXSCNS: Too many frequency'//
+     .                  ' mode or Pcal changes ')
+                    STOP
+                 END IF
 C
-C               IF (IPS .NE. PSETI(ISCN,ISTA)) THEN
-C                  CALL ERRLOG('VXSCNS: All telescopes should '//
-C     .                'use similar PCal setup ')
-C                  STOP
-C               END IF
-            ENDIF
-         END DO
+C                I can only assume this was a leftover Huib v75
 C
-C        In case no station is recording this could have resulted in IFS=0, IPS=0
+C                 IF (IPS .NE. PSETI(ISCN,ISTA)) THEN
+C                    CALL ERRLOG('VXSCNS: All telescopes should '//
+C     .                  'use similar PCal setup ')
+C                    STOP
+C                 END IF
+              ENDIF
+           END DO
 C
-         IF( IPS .EQ. 0 ) IPS = 1
-         IF( IFS .EQ. 0 ) THEN
-            IFS = 1
-            FRSTSCN = FSETSCN(IFS)
-         END IF
+C          In case no station is recording this could have resulted in IFS=0, IPS=0
 C
-C        Account for the telescopes that came in for first time
+           IF( IPS .EQ. 0 ) IPS = 1
+           IF( IFS .EQ. 0 ) THEN
+              IFS = 1
+              FRSTSCN = FSETSCN(IFS)
+           END IF
 C
-         DO ISTA = 1, NSTA
-            IF( STASCN(ISCN,ISTA) ) THEN  
-               IF (VXMDIFP(IFS,IPS) .GT. 0 .AND. 
-     .             FSETSCN(FSETI(ISCN,ISTA)).NE.FRSTSCN .AND. 
-     .             FSETSCN(FSETI(ISCN,ISTA)).EQ.ISCN ) THEN
-                  IF( DEBUG ) THEN
-                     MSGTXT = ' '
-                     WRITE( MSGTXT, '( A, A, A, I3 )' ) 
-     .                   'VXSCNS: Telescope: ', STANAME(ISTA),
-     .                   ' joins mode set in scan: ', 
-     .                   FSETSCN(FSETI(ISCN,ISTA))
-                     CALL WLOG( 1, MSGTXT )
-                  END IF
-                  MODSET(ISTA,VXMDIFP(IFS,IPS)) = MODSET(ISTA,IMODE)
+C          Account for the telescopes that came in for first time
 C
-               END IF
-            END IF
-         END DO
+           DO ISTA = 1, NSTA
+              IF( STASCN(ISCN,ISTA) ) THEN  
+                 IF (VXMDIFP(IFS,IPS) .GT. 0 .AND. 
+     .               FSETSCN(FSETI(ISCN,ISTA)).NE.FRSTSCN .AND. 
+     .               FSETSCN(FSETI(ISCN,ISTA)).EQ.ISCN ) THEN
+                    IF( DEBUG ) THEN
+                       MSGTXT = ' '
+                       WRITE( MSGTXT, '( A, A, A, I3 )' ) 
+     .                     'VXSCNS: Telescope: ', STANAME(ISTA),
+     .                     ' joins mode set in scan: ', 
+     .                     FSETSCN(FSETI(ISCN,ISTA))
+                       CALL WLOG( 1, MSGTXT )
+                    END IF
+                    MODSET(ISTA,VXMDIFP(IFS,IPS)) = MODSET(ISTA,IMODE)
 C
-C        Totally new mode, remeber IFS, IPS are set for one good value
-C        Problem ifs, ips could be first scan unmodified or modified..
+                 END IF
+              END IF
+           END DO
 C
-         IF( VXMDIFP(IFS,IPS) .EQ. 0 ) THEN
+C          Totally new mode, remeber IFS, IPS are set for one good value
+C          Problem ifs, ips could be first scan unmodified or modified..
 C
-            CALSET = SPCAL(FSETKS(IFS))
-            CALSCN = PSPCAL(IPS)
-            CALL UPCASE(CALSET)
-            CALL UPCASE(CALSCN)
-            IF( DEBUG ) THEN
-               MSGTXT = ' '
-               WRITE( MSGTXT, '( A, I3 )' )
-     .             'VXSCNS: Unused or new mode in scan ',ISCN
-               CALL WLOG( 1, MSGTXT )
-            END IF
-            IF( FREQ(1,FRSTSCN).LT.1E-6 .AND. 
-     .          BW(1,FRSTSCN).LT.1E-6 .AND. 
-     .          CALSET .EQ. CALSCN ) 
-     .          THEN
+           IF( VXMDIFP(IFS,IPS) .EQ. 0 ) THEN
+C
+              CALSET = SPCAL(FSETKS(IFS))
+              CALSCN = PSPCAL(IPS)
+              CALL UPCASE(CALSET)
+              CALL UPCASE(CALSCN)
+              IF( DEBUG ) THEN
+                 MSGTXT = ' '
+                 WRITE( MSGTXT, '( A, I3 )' )
+     .               'VXSCNS: Unused or new mode in scan ',ISCN
+                 CALL WLOG( 1, MSGTXT )
+              END IF
+              IF( FREQ(1,FRSTSCN).LT.1E-6 .AND. 
+     .            BW(1,FRSTSCN).LT.1E-6 .AND. 
+     .            CALSET .EQ. CALSCN ) 
+     .            THEN
 C     
-C               Must set all other telescopes too
+C                 Must set all other telescopes too
 C
-               DO ISTA = 1, NSTA
-                  IF( STASCN(ISCN,ISTA) ) THEN
+                 DO ISTA = 1, NSTA
+                    IF( STASCN(ISCN,ISTA) ) THEN
 C     
-                     VXMDIFP(FSETI(ISCN,ISTA),
-     .                   PSETI(ISCN,ISTA)) = IMODE
-                  END IF
-               END DO
-            ELSE
+                       VXMDIFP(FSETI(ISCN,ISTA),
+     .                     PSETI(ISCN,ISTA)) = IMODE
+                    END IF
+                 END DO
+              ELSE
 C
-C               if this is not the original mode?
+C                if this is not the original mode?
 C
-               CALL VXTRAMD(IMODE,IFS,IPS)
-               FRSTSCN = ISCN
-               MODSCN(ISCN) = NMDVEX
+                 CALL VXTRAMD(IMODE,IFS,IPS)
+                 FRSTSCN = ISCN
+                 MODSCN(ISCN) = NMDVEX
 C
-C               Must set all other telescopes too
+C                 Must set all other telescopes too
 C
-               DO ISTA = 1, NSTA
-                  IF( STASCN(ISCN,ISTA) ) THEN
+                 DO ISTA = 1, NSTA
+                    IF( STASCN(ISCN,ISTA) ) THEN
 C     
-                     VXMDIFP(FSETI(ISCN,ISTA),
-     .                   PSETI(ISCN,ISTA)) = NMDVEX
-                  END IF
-               END DO
+                       VXMDIFP(FSETI(ISCN,ISTA),
+     .                     PSETI(ISCN,ISTA)) = NMDVEX
+                    END IF
+                 END DO
 C
-C              list all antennas in this mode
+C                list all antennas in this mode
 C
-               DO ISTA = 1, NSTA  
-                  IF( STASCN(ISCN,ISTA) ) THEN
-                     MODSET(ISTA,NMDVEX) = MODSET(ISTA,IMODE)
-                  END IF
-               END DO
-            END IF
+                 DO ISTA = 1, NSTA  
+                    IF( STASCN(ISCN,ISTA) ) THEN
+                       MODSET(ISTA,NMDVEX) = MODSET(ISTA,IMODE)
+                    END IF
+                 END DO
+              END IF
 C
+           END IF
+C
+C          Any Mode switch
+C
+           IF (FRSTSCN.NE.OLDSCN .OR.
+     .         IPS.NE.OLDIPS) THEN
+              IF( FSETSCN(IFS) .NE. OLDSCN ) THEN 
+                 OLDSCN = FSETSCN(IFS)
+              END IF
+              IF ( OLDIPS .NE. IPS ) THEN 
+                 OLDIPS = IPS
+              END IF 
+           END IF
+           MODSCN(ISCN) = VXMDIFP(IFS,IPS)
          END IF
-C
-C        Any Mode switch
-C
-         IF (FRSTSCN.NE.OLDSCN .OR.
-     .       IPS.NE.OLDIPS) THEN
-            IF( FSETSCN(IFS) .NE. OLDSCN ) THEN 
-               OLDSCN = FSETSCN(IFS)
-            END IF
-            IF ( OLDIPS .NE. IPS ) THEN 
-               OLDIPS = IPS
-            END IF 
-         END IF
-         MODSCN(ISCN) = VXMDIFP(IFS,IPS)
 C
 C        end loop all scans
 C
+      END DO
+C
+C     Now loop through the scans again and replace the MODSCN for any with 
+C     FORMAT=NONE (not processed first time) with the MODSCAN from a recording 
+C     scan.
+C
+      DO ISCN = SCAN1, SCANL
+         ISET = VXGTST( MODSCN(ISCN) )
+         IF( FORMAT(ISET)(1:4) .EQ. 'NONE' ) THEN
+           RECSCN = 0
+C          Need to allow possibility of using a later scan in case there
+C          is no previous recording scan (there must be one recording
+C          scan somewhere)
+           DO JSCN = SCAN1, SCANL
+             IF( RECSCN .EQ. 0 .OR. JSCN .LT. ISCN ) THEN
+               IF( .NOT. NOREC(JSCN) ) THEN
+                 RECSCN = JSCN
+               END IF
+             END IF
+           END DO
+           IF( RECSCN .EQ. 0 ) THEN
+             CALL ERRLOG( 'VXSCNS: there do not appear to be any ' //
+     1       'recording scans in this schedule! Check use of '//
+     2       'NORECORD, POINT and FORMAT=NONE' )
+           ELSE
+             MODSCN(ISCN) = MODSCN(RECSCN)
+           END IF
+         END IF
       END DO
 C     
       RETURN
