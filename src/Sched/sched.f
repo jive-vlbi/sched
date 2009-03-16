540adc5caa53b2e0e26eaa681cf51d5a57e48b40
cwalker <cwalker@d0039845-cc32-0410-9ae4-e027caa9de09>
2009-03-16 04:30:48 +0000

    Moved setting of MAXMOD from Vex area to sched.inc to facilitate making configuration versions without overly inflating the size of some of the bookkeeping arrays in the Vex code.  Not yet fully tested - was working at home and committing so I can pick up at work.  Craig
    git-svn-id: https://svn.aoc.nrao.edu/repos/sched/trunk/sched@186 d0039845-cc32-0410-9ae4-e027caa9de09

--- src/Sched/sched.f
+++ src/Sched/sched.f
@@ -108,7 +108,7 @@ C     It also won't be allowed if the UPTIME mode is in use.
 C
       IF( MKFILES .AND. OPTMODE .NE. 'UPTIME' .AND. .NOT. NOSET ) THEN
 C
-C        Make the global VEX file if needed.
+C        Make the VEX file if needed.
 C
 C        For the moment (9feb2001), the VEX writer cannot deal with
 C        FORMAT=NONE.  Therefore block it, but give Huib a back
@@ -118,6 +118,11 @@ C        format NONE scans.  Those scans will simply be left out of
 C        the Vex file.  Don't pass schedules that are all format NONE
 C        but want VEX.  This will change more in the near future as we
 C        figure out how to do pointing runs with VEX.
+C
+C        As the switch to the DiFX correlator occurs, most interferometry
+C        observations will use VEX.  But block VEX output when doing
+C        OBSTYPE='CONFIG' where there may be a special binary with MSET
+C        = MAXMOD set too small to actually use.
 C
          IF( DOVEX ) THEN
             FMTNONE = .FALSE.
@@ -129,11 +134,14 @@ C
      1              ALLNONE = .FALSE.
             END DO
 C            IF( OVERRIDE .OR. .NOT. FMTNONE ) THEN
-            IF( OVERRIDE .OR. .NOT. ALLNONE ) THEN
+            IF( ( OVERRIDE .OR. .NOT. ALLNONE ) .AND. 
+     1            OBSTYP .NE. 'CONFIG' ) THEN
                CALL VXWRT
             ELSE
-               CALL ERRLOG( 'SCHED: Cannot mix VEX and FORMAT=NONE'//
-     1              ' for all scans in this release.' )
+               WRITE( MSGTXT, '(A,A)' )
+     1           'SCHED: Cannot mix VEX and OBSTYPE=CONFIG ',
+     2           ' or FORMAT=NONE for all scans in this release.'
+               CALL ERRLOG( MSGTXT )
             END IF
          END IF
 C
