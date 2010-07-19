6c2da61e1c731b709a198394138ce7e824b53d34
schedsvn <schedsvn@d0039845-cc32-0410-9ae4-e027caa9de09>
2010-07-19 17:02:02 +0000

    Prevent recording on VLBA Mark5A when FORMAT=NONE.  Useful in RDBE/MARK5C tests. Updocumentation and verification tests. This might be the next version 9.4 beta.
    git-svn-id: https://svn.aoc.nrao.edu/repos/sched/trunk/sched@293 d0039845-cc32-0410-9ae4-e027caa9de09

--- src/Sched/vlbadk.f
+++ src/Sched/vlbadk.f
@@ -5,13 +5,17 @@ C     schedule.  This is pretty simple so far.  Let's hope it
 C     stays that way, but put it here to keep it isolated.
 C
       INCLUDE  'sched.inc'
+      INCLUDE   'schset.inc'
       INTEGER  ISCN, ISTA
       LOGICAL  SETSC
 C ---------------------------------------------------------------------
-C     Write disk=on if recording, else disk=off.
+C     Write disk=on if recording, else disk=off.  Assume FORMAT=NONE
+C     means not recording.  That might happen without NOREC for 
+C     RDBE schedules where the recording is happening outside the 
+C     control of the crd files.
 C
       IF( USEDISK(ISTA) ) THEN
-         IF( NOREC(ISCN) .OR. SETSC ) THEN
+         IF( NOREC(ISCN) .OR. SETSC .OR. FORMAT(LS) .EQ. 'NONE' ) THEN
             WRITE( IUVBA, '( A )' ) 'disk=off'
          ELSE
             WRITE( IUVBA, '( A )' ) 'disk=on'
