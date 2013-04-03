c7c1f8921b05e5f0452aa15f10628abfa56e1594
creynold <creynold@d0039845-cc32-0410-9ae4-e027caa9de09>
2013-04-03 05:44:56 +0000

    Oops, last checkin was premature (quit wrong editor window!). The note should have read:
    Major revision to include a more or less full description of the DDC personality of the DBBC. The PFB personality is assumed to be the same as the RDBE, until I get some decent documentation. Some minor changes to LBA also. This all needs more testing, but appears to work.
    
    - Added 'DBBCVER' keyword to station catalogue to cover the available DBBC versi
    ons. Currently accepts 'astro', 'geo' or hybrid.
    
    - Added bbclba.f and bbcdbbc.f routines to set default BBC->IF mappings for LBA and DBBC respectively. For dbbc the allowed mapping is set in ifdbbc.f so that it can also be called from chkdbbc.f.
    
    - Modified settr.f and vexwrtr.f to respectively set and use the correct channel->bitstream (track) mappings for the MK5B and LBA recorders.
    
    - fixed bug in writing data transfer commands (potential segmentation fault for first scan in schedule).
    
    
    
    
    git-svn-id: https://svn.aoc.nrao.edu/repos/sched/trunk/sched@468 d0039845-cc32-0410-9ae4-e027caa9de09

---
+++ src/Sched/ifdbbc.f
@@ -0,0 +1,64 @@
+      SUBROUTINE IFDBBC( MYDBBCVER, MAXBBC, MAXIF, IFBBC, MIF )
+C
+C     CR: 26 March, 2013
+C     Routine for Sched called by BBCDBBC and CHKDBBC which gives the
+C     allowed IF-> BBC mappings for the different flavours of DBBC.
+C
+      INCLUDE    'sched.inc'
+C
+      CHARACTER   MYDBBCVER*(*)
+      INTEGER     MAXBBC, MAXIF, I
+      INTEGER     IFBBC(MAXBBC, MAXIF), MIF, IIF, IBBC
+C
+C  -----------------------------------------------------------
+C     Software check:
+C
+      IF( DEBUG ) CALL WLOG( 1, 'IFDBBC: Starting' )
+      call wlog(1, 'In ifdbbc.f')
+C
+C     Initialise the IF->BBC lists. IFBBC(IBBC, IIF) = 0 means IBBC not
+C     accessible from IIF. 
+C
+      DO IBBC = 1, MAXBBC
+         DO IIF = 1, MAXIF
+            IFBBC(IBBC, IIF) = 0
+         END DO
+      END DO
+      IF( MYDBBCVER .EQ. 'ASTRO' ) THEN
+C        'Astro' version
+         MIF = 4
+         DO IBBC = 1, 4
+            IFBBC(IBBC, 1) = 1
+            IFBBC(IBBC+4, 2) = 1
+            IFBBC(IBBC+8, 3) = 1
+            IFBBC(IBBC+12, 4) = 1
+         END DO
+      ELSE IF( MYDBBCVER .EQ. 'GEO' ) THEN
+C        'geo' version
+         MIF = 2
+         IFBBC(1, 1) = 1
+         IFBBC(1, 2) = 1
+         DO IBBC = 2, 8
+            IFBBC(IBBC, 1) = 1
+            IFBBC(IBBC+7, 2) = 1
+         END DO
+      ELSE IF( MYDBBCVER .EQ. 'HYBRID' ) THEN
+C        'hybrid' version
+         MIF = 3
+         DO IBBC = 1, 4
+            IFBBC(IBBC, 1) = 1
+            IFBBC(IBBC+4, 2) = 1
+         END DO
+         IFBBC(1, 3) = 1
+         DO IBBC = 2, 8
+            IFBBC(IBBC+7, 3) = 1
+         END DO
+      END IF
+C      DO IBBC=1,MAXBBC
+C         print*, (IFBBC(IBBC,IIF), IIF=1,MAXIF)
+C      END DO
+      
+      call wlog(1, 'leaving ifdbbc.f')
+
+      RETURN
+      END
