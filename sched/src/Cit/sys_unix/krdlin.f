bf73b7cff34acaf930467289705b70f68010e59c
schedsvn <schedsvn@schedsvn>
2014-01-22 20:57:30 +0000

    Some setup changes. Deactivate wrap spec for all but VLBA.
    This is version 11.2, I hope.
    
    Jan. 22, 2014  RCW

--- sched/src/Cit/sys_unix/krdlin.f
+++ sched/src/Cit/sys_unix/krdlin.f
@@ -10,17 +10,29 @@ C substitution.
 C
 C Also turn any line feed characters into blanks to allow reading DOS
 C format files.
+C
+C Protect against crazies who put more than 255 characters in a line.
+C  Jan 2014 RCW.
 C-----------------------------------------------------------------------
       INTEGER LEN1, NSUB, I
       CHARACTER*255 T
+      CHARACTER*260 TL
       CHARACTER     CR*1, BLANK*1
 C
       CR = CHAR( 13 )
       BLANK = ' '
       IF (UNIT.EQ.0) GOTO 340
    10 IF (DIALOG) WRITE (OUTC, '(''* '',$)')
-      READ (UNIT, '(A)', END=340) T
-      MAXREC = LEN1(T)
+      READ (UNIT, '(A)', END=340) TL
+      MAXREC = LEN1(TL)
+      IF( MAXREC .GT. 255 ) THEN
+         WRITE( OUTC, '( A, /, A )' ) 
+     1    'KRDLIN: ERROR - input line longer than 255 characters',
+     2    TL
+         STOP
+      ELSE
+         T = TL
+      END IF
       IF (MAXREC.LT.1) GOTO 10
       IF (REFLEC) WRITE (OUTC, '(1X,A)') T(1:MAXREC)
       DO 20 I = 1, MAXREC
