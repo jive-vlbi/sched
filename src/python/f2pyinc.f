c683b98cd10cebfa203c7cffffa5b41fb1e89d13
Bob Eldering <eldering@jive.eu>
2018-02-15 17:14:29 +0100

    Fortran file only used to force f2py to make interfaces for common blocks

---
+++ src/python/f2pyinc.f
@@ -0,0 +1,8 @@
+      SUBROUTINE F2PYINC ()
+C
+C     Function to force f2py to generate common block interfaces
+C     
+      INCLUDE '../Sched/sched.inc'
+      INCLUDE '../Sched/schset.inc'
+      RETURN
+      END
