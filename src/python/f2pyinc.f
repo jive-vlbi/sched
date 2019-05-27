0bca207520dfc83c5b80353184f191600350b241
Bob Eldering <eldering@jive.eu>
2019-05-27 17:46:26 +0200

    Remove file not used by replacement of make_schedlib.py: setup.py.

--- src/python/f2pyinc.f
+++
@@ -1,8 +0,0 @@
-      SUBROUTINE F2PYINC ()
-C
-C     Function to force f2py to generate common block interfaces
-C     
-      INCLUDE '../Sched/sched.inc'
-      INCLUDE '../Sched/schset.inc'
-      RETURN
-      END
