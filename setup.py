69493b2efa75e8d37228899b2c03a23d623d85a6
Bob Eldering <eldering@jive.eu>
2019-09-27 14:10:21 +0200

    Release the merge with SCHED 11.5 for pip.

--- setup.py
+++ setup.py
@@ -578,7 +578,7 @@ extension = Extension(
 setup(
     cmdclass={"sdist": sdist},
     name="pythonSCHED",
-    version="1.1.1",
+    version="1.2.0",
     author="Bob Eldering",
     author_email="eldering@jive.eu",
     description="Python extension of NRAO's VLBI scheduling program SCHED "
