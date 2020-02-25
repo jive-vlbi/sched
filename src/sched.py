9c4b8d28e75b7395e54dfa9527c60d15f9a186b4
Bob Eldering <eldering@jive.eu>
2020-02-25 17:26:23 +0100

    Improve error message, in particular when a key file is not ended with a '/'.

--- src/sched.py
+++ src/sched.py
@@ -11,6 +11,7 @@ import sys
 import io
 import atexit
 import os
+import traceback
 
 try:
     import readline
@@ -58,107 +59,112 @@ parser.add_argument("-U", "--no_update", action="store_true", default=False,
 
 args = parser.parse_args()
 
-if args.template is None:
-    stdin = sys.stdin
-else:
-    keyin_text = SimpleTemplate(sys.stdin.read()).render()
-    stdin = io.StringIO(keyin_text)
-    if args.template != "":
-        with open(args.template, "w") as f:
-            f.write(keyin_text)
-        stdin.name = args.template
+try:
+    if args.template is None:
+        stdin = sys.stdin
     else:
-        stdin.name = "Rendered template"
+        keyin_text = SimpleTemplate(sys.stdin.read()).render()
+        stdin = io.StringIO(keyin_text)
+        if args.template != "":
+            with open(args.template, "w") as f:
+                f.write(keyin_text)
+            stdin.name = args.template
+        else:
+            stdin.name = "Rendered template"
 
-# workaround for https://github.com/numpy/numpy/issues/9370
-# summary of the different views of strings
-# python: strings are a type of their own, which includes an encoding
-# f2py: returns bytes when fortran would return character*, 
-#       but when assigning a bytes variable to a character*, 
-#       f2py will interpret it as a array of integers, convert each of those 
-#       integers to a string, and then trim that string to 1 character,
-#       for example b"az" -> [97, 122] -> ["97", "122"] -> "91".
-#       assigning a python string to an f2py generated fortran string gives
-#       the correct result
-# fortran: strings are fixed length character arrays, fortran doesn't have a
-#          end-of-string marker, it will just append blanks (spaces) in any
-#          string operation, eg "foo   " == "foo" evaluates to true.
-#          to mimic this behaviour, extend strings with spaces to fortran length
-s.vern.vernum, version = s.versched()
-s.verc.version = bytes(version).decode().ljust(s.verc.version.itemsize)
-s.stmsg()
+    # workaround for https://github.com/numpy/numpy/issues/9370
+    # summary of the different views of strings
+    # python: strings are a type of their own, which includes an encoding
+    # f2py: returns bytes when fortran would return character*, 
+    #       but when assigning a bytes variable to a character*, 
+    #       f2py will interpret it as a array of integers, convert each of those 
+    #       integers to a string, and then trim that string to 1 character,
+    #       for example b"az" -> [97, 122] -> ["97", "122"] -> "91".
+    #       assigning a python string to an f2py generated fortran string gives
+    #       the correct result
+    # fortran: strings are fixed length character arrays, fortran doesn't have a
+    #          end-of-string marker, it will just append blanks (spaces) in any
+    #          string operation, eg "foo   " == "foo" evaluates to true.
+    #          to mimic this behaviour, extend strings with spaces to fortran 
+    #          length
+    s.vern.vernum, version = s.versched()
+    s.verc.version = bytes(version).decode().ljust(s.verc.version.itemsize)
+    s.stmsg()
 
-if not args.no_update:
-    update_catalogs.update_catalogs()
+    if not args.no_update:
+        update_catalogs.update_catalogs()
 
-# update_catalogs can set os.environ["SCHED"], which is used in the 
-# initialization of sched sched modules, so delay the import to here
-from pysched.sched import input_, parameter, schin_module, getfreq, defaults, \
-    vexout, schopt
+    # update_catalogs can set os.environ["SCHED"], which is used in the 
+    # initialization of sched sched modules, so delay the import to here
+    from pysched.sched import input_, parameter, schin_module, getfreq, \
+        defaults, vexout, schopt
 
-if args.freqlist is not None:
-    # first initialize default files stored in fortran common block 
-    # state default values are (value, operator) tuples
-    msgfile_default = schin_module.state_defaults["msgfile"]
-    s.schsco.msgfile = msgfile_default[1](msgfile_default[0]).ljust(
-        s.schsco.msgfile.itemsize)
-    freqfile_default = schin_module.state_defaults["freqfile"]
-    s.schsco.freqfile = freqfile_default[1](freqfile_default[0]).ljust(
-        s.schsco.freqfile.itemsize)
-    s.schcon.freqlist = args.freqlist
-    getfreq()
-    s.wlog(1, "DIVERT:   Frequency table written.  Stopping.")
-    sys.exit(0)
-if args.key is not None:
-    # set default schedule file
-    schin_module.state_defaults["schedule"][0] = args.key
+    if args.freqlist is not None:
+        # first initialize default files stored in fortran common block 
+        # state default values are (value, operator) tuples
+        msgfile_default = schin_module.state_defaults["msgfile"]
+        s.schsco.msgfile = msgfile_default[1](msgfile_default[0]).ljust(
+            s.schsco.msgfile.itemsize)
+        freqfile_default = schin_module.state_defaults["freqfile"]
+        s.schsco.freqfile = freqfile_default[1](freqfile_default[0]).ljust(
+            s.schsco.freqfile.itemsize)
+        s.schcon.freqlist = args.freqlist
+        getfreq()
+        s.wlog(1, "DIVERT:   Frequency table written.  Stopping.")
+        sys.exit(0)
+    if args.key is not None:
+        # set default schedule file
+        schin_module.state_defaults["schedule"][0] = args.key
 
-restart = False
+    restart = False
 
-while True:
-    
-    s.setn1.nset = 0
-    s.srlis.srln = 0
+    while True:
 
-    input_(stdin)
-    print("SCHED:   Processing input.")
-    defaults()
-    s.schpre()
-    s.chksc1()
-    schopt()
-    s.dopfq()
-    s.getsun()
-    s.chkscn()
-    s.schsum(restart)
-    s.fluxh(parameter.ilog, s.schsco.logfile)
-    if s.schcon.plot or args.plot:
-        # initializes matplotlib, so only do when requested
-        from pysched import plot
-        mkfiles, restart = plot.show(restart)
-    else:
-        mkfiles, restart = True, False
-    
-    if restart:
-        s.delscr(restart)
-        s.wlog(0, " ")
-        s.wlog(0, " ===================  RESTART  ===================== ")
-        s.wlog(0, " ")
-    else:
-        break
+        s.setn1.nset = 0
+        s.srlis.srln = 0
 
-if mkfiles and (f2str(s.schsco.optmode) != "UPTIME") and not s.schcon.noset:
-    s.scnrange()
-    s.omsout(restart)
-    if args.old_style_vex:
-        s.vexout()
-    else:
-        vexout()
-    
-    if s.schcon.dovsop:
-        s.vsopwrt()
+        input_(stdin)
+        print("SCHED:   Processing input.")
+        defaults()
+        s.schpre()
+        s.chksc1()
+        schopt()
+        s.dopfq()
+        s.getsun()
+        s.chkscn()
+        s.schsum(restart)
+        s.fluxh(parameter.ilog, s.schsco.logfile)
+        if s.schcon.plot or args.plot:
+            # initializes matplotlib, so only do when requested
+            from pysched import plot
+            mkfiles, restart = plot.show(restart)
+        else:
+            mkfiles, restart = True, False
 
-    s.flags()
-    s.stafiles()
-    
-s.delscr(False)
-s.putout(" -------  Finished  ----------- ")
+        if restart:
+            s.delscr(restart)
+            s.wlog(0, " ")
+            s.wlog(0, " ===================  RESTART  ===================== ")
+            s.wlog(0, " ")
+        else:
+            break
+
+    if mkfiles and (f2str(s.schsco.optmode) != "UPTIME") and not s.schcon.noset:
+        s.scnrange()
+        s.omsout(restart)
+        if args.old_style_vex:
+            s.vexout()
+        else:
+            vexout()
+
+        if s.schcon.dovsop:
+            s.vsopwrt()
+
+        s.flags()
+        s.stafiles()
+
+    s.delscr(False)
+    s.putout(" -------  Finished  ----------- ")
+except Exception as e:
+    s.wlog(1, "Error running pySCHED: {e}".format(e=e))
+    s.wlog(0, traceback.format_exc())
