650a29003693f5d21787647186235e00a593b57e
Bob Eldering <eldering@jive.eu>
2022-04-26 17:38:15 +0200

    When reading setup files, try reading the file from $SCHED/setups if the file does not include a path and it is not found in CWD.

--- src/pysched/sched/getset_module.py
+++ src/pysched/sched/getset_module.py
@@ -4,6 +4,8 @@ from .. import key
 
 import schedlib as s
 
+import os.path
+
 def getset():
     if s.schcon.debug:
         s.wlog(0, "GETSET: Starting.")
@@ -15,10 +17,30 @@ def getset():
         else:
             if setfilename not in \
                (entry.setname for entry in SetupCatalog().read()):
+                success = False
                 try:
                     f = open(setfilename, "r")
+                    success = True
+                except IOError as e:
+                    # try prepending $SCHED/setups
+                    # if the filename does not include a path
+                    try:
+                        path, filename = os.path.split(setfilename)
+                        if path == "":
+                            in_setups = os.path.join(os.environ["SCHED"],
+                                                     "setups",
+                                                     filename)
+                            f = open(in_setups, "r")
+                            success = True
+                    except Exception as e2:
+                        # ignore this exception, report the first one
+                        pass
+                    report = e
                 except Exception as e:
-                    s.wlog(1, str(e))
+                    report = e
+                    pass
+                if not success:
+                    s.wlog(1, str(report))
                     s.errlog("RDSET: Problem opening setup file")
                 with f:
                     input_iterator = key.KeyfileLister(f)
