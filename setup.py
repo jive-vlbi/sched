33662b801bf10276ea39bda50ef471cec030d13e
Bob Eldering <eldering@jive.eu>
2022-05-13 13:47:40 +0200

    Minor release for wrap zone patch.

--- setup.py
+++ setup.py
@@ -602,7 +602,7 @@ extension = Extension(
 setup(
     cmdclass={"sdist": sdist},
     name="pythonSCHED",
-    version="1.23.0",
+    version="1.24.0",
     author="Bob Eldering",
     author_email="eldering@jive.eu",
     description="Python extension of NRAO's VLBI scheduling program SCHED "
