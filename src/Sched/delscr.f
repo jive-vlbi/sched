55cf5defcad7392a613ad701ca7236e728697104
Adriana Escobar De La Torre <aescobar@nrao.edu>
2023-04-05 13:14:20 -0600

    removed advisory message

--- src/Sched/delscr.f
+++ src/Sched/delscr.f
@@ -39,26 +39,26 @@ C
             CALL PUTOUT( '     Please read them.' )
             CALL PUTOUT( ' ' )
 
-            CALL PUTOUT( ' +-----------------------+')
-            CALL PUTOUT( ' | Version 11.8 Advisory |')
-            CALL PUTOUT( ' +-----------------------+')
-            CALL PUTOUT( ' -')
-            CALL PUTOUT( " - Sched v11.7 incorporates the position " // 
-     1                   "changes from 11.5 to 11.6.  ")
-            CALL PUTOUT( " - If you are in a multi epoch observation" //
-     1                   " you may wish to continue using sched 11.5.")
-            CALL PUTOUT( " - Additionally Sched 11.7 uses a new  " //
-     1                   "default srcfile (if no srcfile is set in ")
-            CALL PUTOUT( " - your key file). To ensure " //
-     1                   "sched 11.5 is run by VLBA")
-            CALL PUTOUT( " - operations, please include in the .key " //
-     1                   "file a line similar to:")
-            CALL PUTOUT( " -")
-            CALL PUTOUT( " -     note1 = 'Please schedule with " //
-     1                   "Sched 11.5'")
-            CALL PUTOUT( " -")
-            CALL PUTOUT( " - All other projects should see improved " //
-     1                   "results with this new version of sched. ")
+C            CALL PUTOUT( ' +-----------------------+')
+C            CALL PUTOUT( ' | Version 11.8 Advisory |')
+C            CALL PUTOUT( ' +-----------------------+')
+C            CALL PUTOUT( ' -')
+C            CALL PUTOUT( " - Sched v11.7 incorporates the position " // 
+C     1                   "changes from 11.5 to 11.6.  ")
+C            CALL PUTOUT( " - If you are in a multi epoch observation" //
+C     1                   " you may wish to continue using sched 11.5.")
+C            CALL PUTOUT( " - Additionally Sched 11.7 uses a new  " //
+C     1                   "default srcfile (if no srcfile is set in ")
+C            CALL PUTOUT( " - your key file). To ensure " //
+C     1                   "sched 11.5 is run by VLBA")
+C            CALL PUTOUT( " - operations, please include in the .key " //
+C     1                   "file a line similar to:")
+C            CALL PUTOUT( " -")
+C            CALL PUTOUT( " -     note1 = 'Please schedule with " //
+C     1                   "Sched 11.5'")
+C            CALL PUTOUT( " -")
+C            CALL PUTOUT( " - All other projects should see improved " //
+C     1                   "results with this new version of sched. ")
 
           ELSE
              CALL PUTOUT( ' REMINDER - see ' // 
