93915bf055c541159493ba7dab028e5bc7caab8c
Bob Eldering <eldering@jive.eu>
2025-08-20 15:36:28 +0200

    New minor release for merge with SCHED12.0.

--- setup.py
+++ setup.py
@@ -651,7 +651,7 @@ extension = Extension(
 setup(
     cmdclass={"sdist": sdist},
     name="pythonSCHED",
-    version="1.27.0",
+    version="1.28.0",
     author="Bob Eldering",
     author_email="eldering@jive.eu",
     description="Python extension of NRAO's VLBI scheduling program SCHED "
