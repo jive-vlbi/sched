ffc8181b2a71861ce835ed5e10f04bb3ce65e264
Bob Eldering <eldering@jive.eu>
2026-05-29 14:58:36 +0200

    Add script to normalize reference files.

---
+++ examples/clean_files.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# script to normalize output files from Verify_python to use as reference files
+
+import subprocess, shlex, sys, os
+
+def run(cmd):
+    print(cmd)
+    subprocess.check_call(cmd, shell=True)
+
+if __name__ == "__main__":
+
+    for filename in sys.argv[1:]:
+        print(filename)
+        run(f'sed -i -e "\|Updating catalogs in {os.path.expanduser("~/.pysched")}|d" {filename}')
+        run('sed -i -e "\|Catalogs in {os.path.expanduser("~/.pysched")} are up-to-date.|d" ' + filename)
+        run(f'sed -i -e s:"{os.environ["SCHED"]}":"/home/eldering/sched":g {filename}')
+        run('sed -i -e /"This schedule produced on:"/d ' + filename)
+        run('sed -i -f Check_sed ' + filename)
