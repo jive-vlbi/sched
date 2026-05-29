5b974f83f4db205b2d1cfd300885130bb90f7ffd
Bob Eldering <eldering@jive.eu>
2026-05-29 15:17:23 +0200

    Release adding duplicate STCODE check.

--- setup.py
+++ setup.py
@@ -656,7 +656,7 @@ extension = Extension(
 setup(
     cmdclass={"sdist": sdist},
     name="pythonSCHED",
-    version="1.28.4",
+    version="1.28.5",
     author="Bob Eldering",
     author_email="eldering@jive.eu",
     description="Python extension of NRAO's VLBI scheduling program SCHED "
