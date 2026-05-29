f9d4dd94f1c90ce7bf436d28346cd7d54e59788c
Bob Eldering <eldering@jive.eu>
2026-05-29 15:41:48 +0200

    Fixed missing format directive on string.

--- examples/clean_files.py
+++ examples/clean_files.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     for filename in sys.argv[1:]:
         print(filename)
         run(f'sed -i -e "\|Updating catalogs in {os.path.expanduser("~/.pysched")}|d" {filename}')
-        run('sed -i -e "\|Catalogs in {os.path.expanduser("~/.pysched")} are up-to-date.|d" ' + filename)
+        run(f'sed -i -e "\|Catalogs in {os.path.expanduser("~/.pysched")} are up-to-date.|d" ' + filename)
         run(f'sed -i -e s:"{os.environ["SCHED"]}":"/home/eldering/sched":g {filename}')
         run('sed -i -e /"This schedule produced on:"/d ' + filename)
         run('sed -i -f Check_sed ' + filename)
